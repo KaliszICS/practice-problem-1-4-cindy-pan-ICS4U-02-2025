08203816f334ee10e0633e47141948fbe6c6366d
cindy-pan-ics4u1-02-25 <cindy.pan@student.tdsb.on.ca>
2025-03-17 14:05:51 +0000

    Update

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,6 +1,31 @@
+import java.io.*;
 public class PracticeProblem {
 
-	public static void main(String args[]) {
+	public static String getName(int line, String file){
+		BufferedReader fr = null;
+		String out = "";
 		
+		try{
+			fr = new BufferedReader(new FileReader(file));
+			if (line <=3){
+			for (int i=0; i<line; i++){
+				if ((out=fr.readLine())!=null){
+					out = out.replaceAll("\\d","");
+				}
+			}
+		}
+		} 
+		catch (IOException e){}
+
+		finally {
+			try{
+				if (fr!=null){
+					fr.close();
+				}
+			}
+			catch (IOException e){}
+		}
+		return out.trim();
 	}
+	
 }
