a299cc1a3b20bae676915694e1b89375c2edf03a
cindy-pan-ics4u1-02-25 <cindy.pan@student.tdsb.on.ca>
2025-03-17 14:16:44 +0000

    update

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -28,4 +28,38 @@ public class PracticeProblem {
 		return out.trim();
 	}
 	
+
+
+	public static int getAge(int line, String file){
+		BufferedReader br = null;
+		String out = "";
+		
+		try{
+			br = new BufferedReader(new FileReader(file));
+			if (line <=3){
+			for (int i=0; i<line; i++){
+				if ((out=br.readLine())!=null){
+					out = out.replaceAll("[^\\d]","");
+					out = out.substring(0,2);
+				}
+			}
+		}
+		} 
+		catch (IOException e){}
+
+		finally {
+			try{
+				if (br!=null){
+					br.close();
+				}
+			}
+			catch (Exception e){}
+		}
+		if (out != ""){
+		return Integer.parseInt(out);
+		}
+		else {
+			return -1;
+		}
+	}
 }
